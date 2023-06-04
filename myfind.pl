bf44ed8268235c02c28448fc6055497457e8bfe0
Mikhail Zavoloka <mikhail.zavoloka@gmail.com>
2023-06-05 00:40:11 +0300

    Fix russian case insensitive match

--- myfind.pl
+++ myfind.pl
@@ -2,6 +2,7 @@ use strict;
 use feature 'say';
 use File::Find;
 use Term::ANSIColor;
+use Encode qw( encode_utf8 decode_utf8 );
 
 my @ignore_directories = qw( .git .svn );
 my $i = 0;
@@ -12,9 +13,11 @@ find(\&fill_output_array, ".");
 
 sub fill_output_array
 {
-    my $filename = $_;
+    my $filename = decode_utf8 $_;
+
     for my $argument ( @ARGV )
     {
+        my $argument = decode_utf8($argument);
         if( grep { -d $filename and $filename eq $_; } @ignore_directories )
         {
             $File::Find::prune = 1; return;
@@ -35,7 +38,7 @@ sub output
     my $max_number_length = length $i;
     for my $line ( @output )
     {
-        print color( 'grey5' );
+        print color( 'grey14' );
         print sprintf( "%${max_number_length}u | ", $line -> { 'number' } );
         print( $line -> { 'is_dir' } ? color( 'bold blue' ) : color( 'reset' ) );
         my $slash_for_dir = $line -> { 'is_dir' } ? '/' : '';
